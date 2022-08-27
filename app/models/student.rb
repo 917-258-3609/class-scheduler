fd42c70fd9e7b336dd4e1e76d68ffd643c744880
Kevin Wu <kevinwu2@andrew.cmu.edu>
2022-08-27 14:06:47 -0400

    Sanitize student and user input

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,18 +1,20 @@
 class Student < ApplicationRecord
     include PgSearch::Model
-
+    # Associations
     has_and_belongs_to_many :courses, optional: true
     has_one :schedule, as: :scheduleable, dependent: :destroy
     has_one :user, as: :accountable, dependent: :destroy
-
+    # Validation
     validates_presence_of :schedule
     validates_presence_of :user
     validate :schedule_preference_recur_indefinitely
     validate :schedule_preference_is_weekly
-
+    # Scope
     pg_search_scope :search_by_full_name, 
         against: [:first_name, :last_name],
         using: :trigram 
+    # Callback
+    before_validation :sanitize
 
     def name
         return "#{self.first_name} #{self.last_name}"
@@ -23,5 +25,9 @@ class Student < ApplicationRecord
     end
     def schedule_preference_recur_indefinitely
         self.schedule.occurrences.all.all?{|o|o.count.nil?}
+    end
+    def sanitize
+        self.first_name.capitalize!
+        self.last_name.capitalize!
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ApplicationRecord
     validates_uniqueness_of :email, case_sensitive: false
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
     
+    before_validation :sanitize
     def teacher
         accountable if accountable.is_a? Teacher
     end
     def student 
         accountable if accountable.is_a? Student
+    end
+    private
+    def sanitize
+        self.email.downcase!
     end
 end
