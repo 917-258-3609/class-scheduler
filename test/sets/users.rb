a29d2a7f002e1ddef5930e9e61117d7ce54d9fe8
Kevin Wu <kevinwu2@andrew.cmu.edu>
2022-08-30 11:00:29 -0400

    Change formatting

--- test/sets/users.rb
+++ test/sets/users.rb
@@ -1,14 +1,9 @@
 module ConstructionHelper
   module Users 
     def create_users
-      @default_user = FactoryBot.create(:user, 
-                                                email: "defaultno@gmail.com")
-      @inactive_user = FactoryBot.create(:user, 
-                                                 email: "inactive@gmail.com", 
-                                                 is_active: false)
-      @rich_user = FactoryBot.create(:user, 
-                                             email: "rich@gmail.com", 
-                                             balance: 10000)
+      @default_user = FactoryBot.create(:user, email: "defaultno@gmail.com")
+      @inactive_user = FactoryBot.create(:user, email: "inactive@gmail.com", is_active: false)
+      @rich_user = FactoryBot.create(:user, email: "rich@gmail.com", balance: 10000)
     end 
     def destroy_users
       @default_teacher_user.delete
