858ca9b97b30722e2630be936b8174e60e2cf9cb
Kevin Wu <kevinwu2@andrew.cmu.edu>
2022-09-12 01:19:41 -0400

    Update course test factory to assign name attribute

--- test/factories/courses.rb
+++ test/factories/courses.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     fee { 999 }
     location { "Zoom" }
     comment { "#{subject_level.level_name} level #{subject_level.subject} class taught by #{teacher.name}" }
+    name { "#{subject_level.to_s} #{teacher.name}" }
     is_active { true }
     teacher
     subject_level
