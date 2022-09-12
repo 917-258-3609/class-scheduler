421d6c3d1a801550015b233f50841161234df29f
Kevin Wu <kevinwu2@andrew.cmu.edu>
2022-09-12 10:28:18 -0400

    Add test for total_teaching_hours_between

--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -11,7 +11,17 @@ class TeacherTest < ActiveSupport::TestCase
     #   destroy_teachers
     #   destroy_subject_levels
     # end
-    should "create and destroy teachers" do
+    should "have total_teaching_hours_between that summarize total teaching time" do
+      create_schedules
+      create_occurrences
+      create_courses
+
+      btime = Time.parse("2022-08-01 14:00:00")
+      etime = Time.parse("2022-08-06 14:00:00")
+      
+      assert_equal(4.0, @peach.total_teaching_hours_between(
+        btime, etime
+      ))
     end
   end
 end
